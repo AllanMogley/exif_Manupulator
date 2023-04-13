92961950662ffbc4b6c23ac9e6b3ed85468a2ac8
Allan Mogley <120258354+AllanMogley@users.noreply.github.com>
2023-04-13 14:07:39 +0300

    Added Libraries

--- exif Reader.R
+++ exif Reader.R
@@ -1,15 +1,19 @@
 # Load exiftoolr package
 library(exiftoolr)
 library(dplyr)
+library(visdat)
+library(inspectdf)
 
 # Read multi-band TIFF file
 tiff_file <- list.files(pattern = "*.TIF")
 metadata <- exif_read(tiff_file)
 
-# Display band names
-cat("Band names:\n")
-cat(metadata$BandName)
-print(metadata$FileName, metadata$BandName)
 
 table(metadata$BandFreq)
 table(metadata$BandName)
+
+head(metadata, 5)
+summary(metadata)
+inspect_cat(metadata)
+
+vis_dat(metadata)
