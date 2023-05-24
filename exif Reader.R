23d622729b15f89aef848e0c88a62030e42128c2
Allan Mogley <allanmogley@gmail.com>
2023-05-24 10:37:41 +0300

    Added Data Inspection Lines

--- exif Reader.R
+++ exif Reader.R
@@ -1,19 +1,46 @@
-# Load exiftoolr package
-library(exiftoolr)
-library(dplyr)
-library(visdat)
-library(inspectdf)
+# Install Load packages if absent----------
+if(!require(pacman)) install.packages("pacman")
+pacman::p_load(exiftoolr,
+               dplyr,
+               visdat,
+               inspectdf,
+               tidyverse, # meta-package
+               inspectdf,
+               plotly,
+               janitor,
+               visdat,
+               esquisse)
 
-# Read multi-band TIFF file
-tiff_file <- list.files(pattern = "*.TIF")
+
+
+
+# Read multi-band TIFF file --------------------
+tiff_file <- list.files(pattern = "*.TIF") [1:10]
 metadata <- exif_read(tiff_file)
+
+
+#Inspect the Data Frame-----------
+head(metadata, 5)
+summary(metadata)
+
+show_plot(inspect_cat(metadata))
+show_plot(inspect_num(metadata))
+vis_dat(metadata)
+
+# Select and Subset Numeric Columns
+num_data = dplyr::select(metadata, where(is.numeric))
+vis_cor(num_data)
+vis_dat(num_data)
+
+# vis_miss(metadata)
 
 
 table(metadata$BandFreq)
 table(metadata$BandName)
 
-head(metadata, 5)
-summary(metadata)
-inspect_cat(metadata)
 
-vis_dat(metadata)
+
+band_name 
+
+
+
